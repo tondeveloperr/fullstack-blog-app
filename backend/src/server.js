7ffc1e0a39702efc70b8ff92d5df7b820e744e2d
tonbots <kartono.saleh@gmail.com>
2023-07-27 07:07:07 +0700

    Menambahkan multer disk storage dan file upload endpoint

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,10 +4,12 @@ dotenv.config();
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import corsOptions from "./utils/corsOptions.js";
+import multer from "multer";
 
 import authRoute from "./routes/authRoute.js";
 import userRoute from "./routes/userRoute.js";
 import postRoute from "./routes/postRoute.js";
+import { Hello } from "../Hello.js";
 
 const port = process.env.APP_PORT || 5000;
 
@@ -17,6 +19,22 @@ app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(cookieParser());
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "../frontend/public/upload");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+app.post("/api/v1/upload", upload.single("file"), function (req, res) {
+  const file = req.file;
+  res.status(200).json(file.filename);
+});
 
 app.use("/api/v1", authRoute);
 app.use("/api/v1", userRoute);
