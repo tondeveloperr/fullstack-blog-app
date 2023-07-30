40126b1e9804271f07329ac85dbf52877565baf7
tonbots <kartono.saleh@gmail.com>
2023-07-30 23:15:59 +0700

    Membuat halaman Profile dan komponen untuk edit profile. serta menerapkan API user

---
+++ frontend/src/components/atoms/modalEditProfile/ModalEditProfile.jsx
@@ -0,0 +1,130 @@
+import { useContext, useState } from "react";
+import { AuthContext } from "../../../context/AuthContext";
+import PropTypes from "prop-types";
+import "./modalEditProfile.scss";
+import axiosInstance from "../../../utils/axiosConfig";
+
+const ModalEditProfile = ({ onClose, onUpdateProfile }) => {
+  const { currentUser } = useContext(AuthContext);
+  const [formData, setFormData] = useState({
+    username: currentUser.username,
+    email: currentUser.email,
+    image: null, // Simpan file gambar yang akan diunggah
+  });
+
+  const handleChange = (e) => {
+    const { name, value, type } = e.target;
+
+    // If the input is a file input (for image), handle it separately
+    if (type === "file") {
+      setFormData((prevFormData) => ({
+        ...prevFormData,
+        image: e.target.files[0], // Simpan file gambar yang akan diunggah
+      }));
+    } else {
+      setFormData((prevFormData) => ({
+        ...prevFormData,
+        [name]: value,
+      }));
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    let imageUrl = currentUser.image; // Default image URL, in case no new image is uploaded
+
+    if (formData.image) {
+      // Jika ada gambar yang diunggah, upload gambar dan dapatkan URL gambar baru
+      imageUrl = await uploadImage(formData.image);
+
+      // Pastikan Anda memeriksa apakah imageUrl ada sebelum memperbarui formData
+      if (imageUrl) {
+        // Update data profil pengguna dengan URL gambar baru
+        setFormData((prevFormData) => ({
+          ...prevFormData,
+          image: imageUrl, // Set URL gambar baru
+        }));
+      }
+    }
+
+    // Update data profil pengguna dengan URL gambar baru
+    const updatedData = { ...formData, image: imageUrl };
+    onUpdateProfile(updatedData);
+    onClose();
+  };
+
+  const uploadImage = async (imageFile) => {
+    try {
+      const formDataToSend = new FormData();
+      formDataToSend.append("file", imageFile);
+
+      const response = await axiosInstance.post("/upload", formDataToSend);
+      console.log(response);
+      return response.data;
+    } catch (error) {
+      console.log("Error uploading image:", error);
+      return null;
+    }
+  };
+
+  return (
+    <div className="modal">
+      <div className="modal-content">
+        <h2>Edit Profile</h2>
+        <form onSubmit={handleSubmit} encType="multipart/form-data">
+          <div className="form-group">
+            <label htmlFor="username">Username:</label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              value={formData.username}
+              onChange={handleChange}
+              className="input-field"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              className="input-field"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="image">Profile Image:</label>
+            <input
+              type="file"
+              id="image"
+              name="image"
+              accept="image/*"
+              onChange={handleChange}
+              className="input-field"
+            />
+            {/* Add preview image here if needed */}
+          </div>
+          {/* Add other fields that can be edited here */}
+          <div className="form-actions">
+            <button type="submit" className="submit-btn">
+              Save Changes
+            </button>
+            <button type="button" onClick={onClose} className="cancel-btn">
+              Cancel
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+ModalEditProfile.propTypes = {
+  onClose: PropTypes.func.isRequired,
+  onUpdateProfile: PropTypes.func.isRequired,
+};
+
+export default ModalEditProfile;
---
+++ frontend/src/pages/profile/Profile.jsx
@@ -0,0 +1,114 @@
+import { useContext, useEffect, useState } from "react";
+import { IconAvatarDefault } from "../../assets/icons";
+import "./profile.scss";
+import { AuthContext } from "../../context/AuthContext";
+import axiosInstance from "../../utils/axiosConfig";
+import { useNavigate } from "react-router-dom";
+import { ModalEditProfile } from "../../components/atoms";
+
+const Profile = () => {
+  const { currentUser, setCurrentUser } = useContext(AuthContext);
+  const [userPosts, setUserPosts] = useState([]);
+  const [errMessage, setErrMessage] = useState("");
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // Buat fungsi untuk mengambil data postingan berdasarkan `uid` current user
+    const fetchPosts = async () => {
+      try {
+        const response = await axiosInstance.get(
+          `/userPosts?uid=${currentUser?.id}`
+        );
+        setUserPosts(response.data);
+      } catch (error) {
+        if (error.response) {
+          setErrMessage(error.response?.data?.message);
+        }
+      }
+    };
+
+    fetchPosts();
+  }, [currentUser?.id]);
+
+  const getText = (html) => {
+    const document = new DOMParser().parseFromString(html, "text/html");
+    return document.body.textContent;
+  };
+
+  const navigateToPost = (postId) => {
+    navigate(`/post/${postId}`);
+  };
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  // Function to handle closing the modal
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleUpdateProfile = async (updatedData) => {
+    try {
+      await axiosInstance.put(`/user/${currentUser?.id}`, updatedData);
+      setCurrentUser((prevUser) => ({ ...prevUser, ...updatedData }));
+      //   console.log(setCurrentUser);
+      closeModal();
+    } catch (error) {
+      if (error.response) {
+        console.log(error.response.data.message);
+      }
+    }
+  };
+
+  return (
+    <div className="container-profile">
+      <div className="background">
+        <div className="wrapper">
+          <div className="profile-user">
+            {currentUser?.image ? (
+              <img src={`../upload/${currentUser?.image}`} alt="Profile" />
+            ) : (
+              <img src={IconAvatarDefault} alt="Profile" />
+            )}
+          </div>
+          <div className="profile-info">
+            <div>
+              <h2>{currentUser?.username}</h2>
+              <p>{currentUser?.email}</p>
+            </div>
+            <button onClick={openModal}>Edit Profile</button>
+          </div>
+        </div>
+      </div>
+      <div className="box-list">
+        <div className="list-post">
+          <h2 className="label">Your Posts:</h2>
+          {userPosts.length === 0 ? (
+            <p className="no-post">{errMessage}</p>
+          ) : (
+            userPosts.map((post) => (
+              <div className="post" key={post.id}>
+                <img src={`../upload/${post.image}`} alt="img" />
+                <div className="wrapper">
+                  <h2 onClick={() => navigateToPost(post.id)}>{post.title}</h2>
+                  <p>{getText(post.description)}</p>
+                </div>
+              </div>
+            ))
+          )}
+        </div>
+      </div>
+      {isModalOpen && (
+        <ModalEditProfile
+          onClose={closeModal}
+          onUpdateProfile={handleUpdateProfile}
+        />
+      )}
+    </div>
+  );
+};
+
+export default Profile;
