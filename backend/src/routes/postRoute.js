0e09983132e9cf4cc6f322d3d0fbc905eae1956a
tonbots <kartono.saleh@gmail.com>
2023-07-30 22:57:55 +0700

    menambahkan post controller di route

--- backend/src/routes/postRoute.js
+++ backend/src/routes/postRoute.js
@@ -4,12 +4,14 @@ import {
   deletePost,
   getPost,
   getPosts,
+  getUserPosts,
   updatePost,
 } from "../controllers/postController.js";
 const router = express.Router();
 
 router.get("/posts", getPosts);
 router.get("/post/:id", getPost);
+router.get("/userPosts", getUserPosts);
 router.post("/post", addPost);
 router.put("/post/:id", updatePost);
 router.delete("/post/:id", deletePost);
