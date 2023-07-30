44b99c679241085ce889603e738ec9dd039c204b
tonbots <kartono.saleh@gmail.com>
2023-07-31 06:34:24 +0700

    Mengapus beberapa File yang sudah tidak di perlukan

--- frontend/src/assets/icons/page.jsx
+++
@@ -1,6 +0,0 @@
-import IconDelete from "./delete.png";
-import IconEdit from "./edit.png";
-import IconLogo from "./logo.png";
-import IconAvatarDefault from "./avatar-default.png";
-
-export { IconDelete, IconEdit, IconLogo, IconAvatarDefault };
